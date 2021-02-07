d13e35f00ca66de96f08dfafb322be22caf74593
OlegVz <vozniukos@gmail.com>
2021-02-07 16:37:39 +0200

    - add ProductFacade class;

---
+++ src/main/java/com/hybris/shop/facade/impl/ProductFacade.java
@@ -0,0 +1,68 @@
+package com.hybris.shop.facade.impl;
+
+import com.hybris.shop.dto.NewProductDto;
+import com.hybris.shop.dto.ProductDto;
+import com.hybris.shop.exceptions.productExceptions.ProductNotFoundByIdException;
+import com.hybris.shop.facade.ProductFacadeInterface;
+import com.hybris.shop.mapper.ProductMapper;
+import com.hybris.shop.model.Product;
+import com.hybris.shop.service.impl.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ProductFacade implements ProductFacadeInterface {
+
+    private final ProductService productService;
+
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public ProductFacade(ProductService productService, ProductMapper productMapper) {
+        this.productService = productService;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public ProductDto save(NewProductDto newProductDto) {
+        Product savedProduct = productService.save(productMapper.fromNewProductDtoToEntity(newProductDto));
+
+        return productMapper.fromEntityToProductDto(savedProduct);
+    }
+
+    @Override
+    public ProductDto findById(Long id) {
+        return productMapper.fromEntityToProductDto(productService.findById(id));
+    }
+
+    @Override
+    public ProductDto update(Long id, NewProductDto newProductDto) {
+        if (!productService.existsById(id)) {
+            throw new ProductNotFoundByIdException(id);
+        }
+
+        Product updatedProduct = productService.update(id, productMapper.fromNewProductDtoToEntity(newProductDto));
+
+        return productMapper.fromEntityToProductDto(updatedProduct);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return productService.existsById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productService.deleteById(id);
+    }
+
+    @Override
+    public List<ProductDto> findAll() {
+        return productService.findAll().stream()
+                .map(productMapper::fromEntityToProductDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/hybris/shop/mapper/ProductMapper.java
@@ -0,0 +1,27 @@
+package com.hybris.shop.mapper;
+
+import com.hybris.shop.dto.NewProductDto;
+import com.hybris.shop.dto.ProductDto;
+import com.hybris.shop.model.Product;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class ProductMapper {
+
+    private ModelMapper modelMapper;
+
+    public ProductMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public Product fromNewProductDtoToEntity(NewProductDto newProductDto) {
+        return Objects.isNull(newProductDto) ? null : modelMapper.map(newProductDto, Product.class);
+    }
+
+    public ProductDto fromEntityToProductDto(Product product) {
+        return Objects.isNull(product) ? null : modelMapper.map(product, ProductDto.class);
+    }
+}
