31347b637405beaca81aff63ebabe5b900c813ed
OlegVz <vozniukos@gmail.com>
2021-03-21 16:17:52 +0200

    tmp

--- src/main/java/com/hybris/shop/servlets/IsUserExistServlet.java
+++ src/main/java/com/hybris/shop/servlets/IsUserExistServlet.java
@@ -1,13 +1,18 @@
 package com.hybris.shop.servlets;
 
 import com.hybris.shop.facade.impl.UserFacade;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -19,9 +24,22 @@ public class IsUserExistServlet extends HttpServlet {
     public IsUserExistServlet() {
     }
 
+    private WebApplicationContext springContext;
+
     @Autowired
     public IsUserExistServlet(UserFacade userFacade) {
         this.userFacade = userFacade;
+    }
+
+    @Override
+    public void init(final ServletConfig config) throws ServletException {
+            super.init(config);
+            SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
+                    config.getServletContext());
+//        super.init(config);
+//        springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext());
+//        final AutowireCapableBeanFactory beanFactory = springContext.getAutowireCapableBeanFactory();
+//        beanFactory.autowireBean(this);
     }
 
     @Override
@@ -34,7 +52,6 @@ public class IsUserExistServlet extends HttpServlet {
         PrintWriter pw = response.getWriter();
         pw.println("<html>");
         pw.println("<h1> Is user with email exist: " + b + "</h1>");
-        pw.println("<h1> Is user with email exist: </h1>");
         pw.println("</html>");
     }
 }
