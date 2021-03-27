1983e1532c061153077f990ca14d56013b405df1
OlegVz <vozniukos@gmail.com>
2021-03-27 12:26:16 +0200

    refactor code

--- src/main/java/com/hybris/shop/ShopApplication.java
+++ src/main/java/com/hybris/shop/ShopApplication.java
@@ -1,9 +1,7 @@
 package com.hybris.shop;
 
-import com.hybris.shop.view.menu.MainMenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
